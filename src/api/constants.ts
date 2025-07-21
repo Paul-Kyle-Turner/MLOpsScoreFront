2cd5447bf3b498712f7bde3c22cdae2379fd71ca
PaulKTurner <paul@handshake.fyi>
2025-07-21 09:20:47 -0400

    build dependent constant

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,2 +1,10 @@
 
-export const API_BASE_URL = 'http://127.0.0.1:8000'; // Replace with your actual API base URL
+export const API_BASE_URL = (() => {
+    if (import.meta.env.MODE === 'production') {
+        return 'https://mlopsscoreback-720438592859.us-central1.run.app';
+    } else if (import.meta.env.MODE === 'staging') {
+        return 'https://mlopsscoreback-720438592859.us-central1.run.app';
+    } else {
+        return 'http://127.0.0.1:8000'; // must be http for local development
+    }
+})();
